2846b8332fb3f8ae17664217524c87ce075b1a43
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-05-26 06:28:25 +0530

    feat: enhance Button component to support variant styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,28 @@
-import type { ReactNode } from "react";
+import type { ComponentProps } from "react";
 
+type Variant = "primary" | "secondary" | "ghost-destructive";
 type ButtonProps = {
-  children: ReactNode;
+  variant?: Variant;
+} & ComponentProps<"button">;
+
+export const Button = ({ variant = "primary", ...props }: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={`${getVariantStyles(variant)} transition-colors rounded px-2 disabled:opacity-30 disabled:cursor-not-allowed`}
+    />
+  );
 };
 
-export const Button = ({ children }: ButtonProps) => {
-  return <button className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 disabled:opacity-30 disabled:cursor-not-allowed">{children}</button>;
-};
+function getVariantStyles(variant: Variant) {
+  switch (variant) {
+    case "primary":
+      return "bg-violet-600 hover:bg-violet-500";
+    case "secondary":
+      return "bg-zinc-700 hover:bg-zinc-600";
+    case "ghost-destructive":
+      return "hover:bg-red-800/10 text-red-800 hover:text-red-200";
+    default:
+      throw new Error(`Unknown variant: ${variant satisfies never}`);
+  }
+}
