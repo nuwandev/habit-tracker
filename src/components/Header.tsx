d10cb2dc2503ec32646d91e11f6c75b086b3aac6
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-05-31 18:24:19 +0530

    feat: enhance Header component to accept dynamic date range and navigation callbacks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,23 @@
-import { isToday } from "date-fns";
+import { format, isToday } from "date-fns";
 import { useHabits } from "../context/useHabits";
 import { Button } from "./Button";
+type HeaderProps = {
+  visibleDates: Date[];
+  onPrev: () => void;
+  onNext: () => void;
+};
 
-const Header = () => {
+const Header = ({ visibleDates, onNext, onPrev }: HeaderProps) => {
   const { habits } = useHabits();
 
   const doneToday = habits.filter((h) =>
     h.completions.some((c) => isToday(c)),
   ).length;
+
+  const dateRange = `${format(visibleDates[0], "MMM d")} - ${format(
+    visibleDates[visibleDates.length - 1],
+    "MMM d",
+  )}`;
 
   return (
     <header className="flex items-center justify-between">
@@ -19,10 +29,15 @@ const Header = () => {
       </div>
 
       <div className="flex flex-col gap-1 items-end">
-        <span className="text-zinc-400 text-sm">Apr 6 - Apr 12</span>
+        <span className="text-zinc-400 text-sm">{dateRange}</span>
         <div className="flex items-center gap-3">
-          <Button>Prev</Button>
-          <Button>Next</Button>
+          <Button onClick={onPrev}>Prev</Button>
+          <Button
+            onClick={onNext}
+            disabled={visibleDates.some((d) => isToday(d))}
+          >
+            Next
+          </Button>
         </div>
       </div>
     </header>
